f2ef3d6492d23c22e7054641c452539c41f7b190
S M Purnima Wijendra <thepurryness@Ss-MacBook-Air.local>
2016-05-03 22:27:05 +0800

    change db at staging

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,20 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+    # # General Settings
+    # config.app_domain = 'somedomain.com'
+    #
+    # # Email
+    # config.action_mailer.delivery_method = :smtp
+    # config.action_mailer.perform_deliveries = true
+    # config.action_mailer.default_url_options = { host: config.app_domain }
+    # config.action_mailer.smtp_settings = {
+    #   address: 'smtp.gmail.com',
+    #   port: '587',
+    #   enable_starttls_auto: true,
+    #   user_name: 'someuser',
+    #   password: 'somepass',
+    #   authentication: :plain,
+    #   domain: 'somedomain.com'
+    # }
