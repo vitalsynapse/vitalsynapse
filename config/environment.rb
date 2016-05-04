c33c849eec188f04116e75b1ff9491293e2e4f55
S M Purnima Wijendra <thepurryness@Ss-MacBook-Air.local>
2016-05-04 21:12:32 +0800

    added environment host

--- config/environment.rb
+++ config/environment.rb
@@ -4,19 +4,18 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-    # # General Settings
-    # config.app_domain = 'somedomain.com'
-    #
-    # # Email
-    # config.action_mailer.delivery_method = :smtp
-    # config.action_mailer.perform_deliveries = true
-    # config.action_mailer.default_url_options = { host: config.app_domain }
-    # config.action_mailer.smtp_settings = {
-    #   address: 'smtp.gmail.com',
-    #   port: '587',
-    #   enable_starttls_auto: true,
-    #   user_name: 'someuser',
-    #   password: 'somepass',
-    #   authentication: :plain,
-    #   domain: 'somedomain.com'
-    # }
+    # General Settings
+    config.app_domain = 'vitalsynapse-staging.herokuapp.com'
+
+    # Email
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.default_url_options = { host: https://vitalsynapse-staging.herokuapp.com/ }
+    config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: '587',
+      enable_starttls_auto: true,
+
+      authentication: :plain,
+      domain: 'vitalsynapse-staging.herokuapp.com'
+    }
