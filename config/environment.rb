4a49443897ac6d7da417e288096d2b4c7f4de9bd
S M Purnima Wijendra <thepurryness@Ss-MacBook-Air.local>
2016-05-09 19:52:20 +0800

    comment out environment mailer settings

--- config/environment.rb
+++ config/environment.rb
@@ -4,18 +4,19 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
+#dont need mail settings here since its already in production.rb
     # General Settings
-    config.app_domain = 'vitalsynapse-staging.herokuapp.com'
-
-    # Email
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.perform_deliveries = true
-    config.action_mailer.default_url_options = { host: vitalsynapse-staging.herokuapp.com }
-    config.action_mailer.smtp_settings = {
-      address: 'smtp.gmail.com',
-      port: '587',
-      enable_starttls_auto: true,
-
-      authentication: :plain,
-      domain: 'vitalsynapse-staging.herokuapp.com'
-    }
+    # config.app_domain = 'vitalsynapse-staging.herokuapp.com'
+    #
+    # # Email
+    # config.action_mailer.delivery_method = :smtp
+    # config.action_mailer.perform_deliveries = true
+    # config.action_mailer.default_url_options = { host: vitalsynapse-staging.herokuapp.com }
+    # config.action_mailer.smtp_settings = {
+    #   address: 'smtp.gmail.com',
+    #   port: '587',
+    #   enable_starttls_auto: true,
+    #
+    #   authentication: :plain,
+    #   domain: 'vitalsynapse-staging.herokuapp.com'
+    # }
