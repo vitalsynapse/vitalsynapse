d565dcf02d8b86a679b3d5ec555b8467a3709df7
purnima23 <purry235@gmail.com>
2016-05-12 14:08:49 +0800

    added form

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,6 +23,7 @@ Answer.find_or_create_by(question_id: 1, description:  "Hypertension")
 Answer.find_or_create_by(question_id: 1, description:  "Heart Disease")
 Answer.find_or_create_by(question_id: 1, description:  "Bronchial Asthma")
 Answer.find_or_create_by(question_id: 1, description:  "Cancer")
+Answer.find_or_create_by(question_id: 1, description:  "Others")
 
 Answer.find_or_create_by(question_id: 2, description:  "Mother")
 Answer.find_or_create_by(question_id: 2, description:  "Father")
@@ -30,6 +31,7 @@ Answer.find_or_create_by(question_id: 2, description:  "Grandpa")
 Answer.find_or_create_by(question_id: 2, description:  "Grandma")
 Answer.find_or_create_by(question_id: 2, description:  "Siblings")
 Answer.find_or_create_by(question_id: 2, description:  "None")
+
 
 Answer.find_or_create_by(question_id: 3, description:  "Smoke")
 Answer.find_or_create_by(question_id: 3, description:  "Consume Alcohol")
@@ -41,5 +43,7 @@ Answer.find_or_create_by(question_id: 4, description:  "No, I usually do not exe
 Answer.find_or_create_by(question_id: 5, description:  "Drugs")
 Answer.find_or_create_by(question_id: 5, description:  "Food")
 Answer.find_or_create_by(question_id: 5, description:  "Dust")
+Answer.find_or_create_by(question_id: 5, description:  "Others")
 
 Answer.find_or_create_by(question_id: 6, description:  "No")
+Answer.find_or_create_by(question_id: 6, description:  "Yes")
