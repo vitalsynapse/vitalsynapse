d09809742879e243e5ba7e34b9ca8969d02c6842
purnima23 <purry235@gmail.com>
2016-05-21 21:16:56 +0800

    initializers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'vitalsynapse.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'www.app.vitalsynapse.com' }
 
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
