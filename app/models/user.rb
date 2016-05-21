51ebbbfddf1ae06fc7ec3680ef0be40f236eb630
purnima23 <purry235@gmail.com>
2016-05-21 12:18:41 +0800

    devise password

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,7 +50,7 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      # user.password = Devise.friendly_token[0,20]
+      user.password = Devise.friendly_token[0,20]
       user.first_name = auth.info.name   # assuming the user model has a name
        user.profile_pic = auth.info.image # assuming the user model has an image
     end
