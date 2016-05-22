719d07b95a9161b0a82aba55fa7f59d8e91e9ee1
purnima23 <purry235@gmail.com>
2016-05-22 14:56:02 +0800

    precompile

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => ENV['S3_KEY'],
-    :aws_secret_access_key  => ENV['S3_SECRET']
+    :aws_access_key_id      => "AKIAI4E7K7BI55OA47UQ",
+    :aws_secret_access_key  => "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"
   }
   config.fog_directory  = 'vitalsynapse.userprofile'
 
