40d72a3f1833be750c96070294ad0470a81c257b
purnima23 <purry235@gmail.com>
2016-05-24 12:46:17 +0800

    changes

--- app/uploaders/health_record_uploader.rb
+++ app/uploaders/health_record_uploader.rb
@@ -8,7 +8,7 @@ class HealthRecordUploader < CarrierWave::Uploader::Base
   # mount_uploader :file, PictureUploader
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+   storage :file
   #  storage :fog
 
   # Override the directory where uploaded files will be stored.
